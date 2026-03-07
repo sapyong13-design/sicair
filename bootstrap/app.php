b23361e8b3fca8d06c3099c45e461b3ccb4d32aa
Faris <sapyong13@gmail.com>
2026-03-07 15:44:07 +0700

    security: SecurityHeaders middleware (X-Frame-Options, X-Content-Type-Options, dll)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\IsAdmin::class,
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
